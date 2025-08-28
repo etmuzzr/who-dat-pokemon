61e47270c775b617022dc30d3bd1168eca7cef5e
Ethan <ethanmurray180@gmail.com>
2025-08-28 21:58:33 +0100

    5th attempt gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Home from "./Pages/Home.jsx"
 import Pokedex from "./Pages/Pokedex.jsx"
 import Play from "./Pages/Play.jsx"
 
 function App() {
   return (
-    <Router basename="/">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pokedex" element={<Pokedex />} />
-        <Route path="/play" element={<Play />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/pokedex" element={<Pokedex />} />
+      <Route path="/play" element={<Play />} />
+    </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { React, StrictMode } from "react";
+import { HashRouter} from "react-router-dom"
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <HashRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </HashRouter>
 );
 
