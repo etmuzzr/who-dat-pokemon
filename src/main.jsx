2d9f178d2bab7410a12daf1b0e0571e387a2499a
Ethan <ethanmurray180@gmail.com>
2025-08-28 22:00:46 +0100

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <StrictMode>
+  <StrictMode>
+    <HashRouter>
       <App />
-    </StrictMode>
-  </HashRouter>
+    </HashRouter>
+  </StrictMode>
 );
 
