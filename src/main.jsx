c25e2f340af8b14d90a59f19608541862faa943a
Ethan <ethanmurray180@gmail.com>
2025-08-28 21:21:37 +0100

    first attempt deploy gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { React, StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter> */}
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
