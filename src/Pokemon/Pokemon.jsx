fcb9069b354625afa6ae35f9b631f8518e06ad54
Ethan <ethanmurray180@gmail.com>
2025-08-30 16:51:52 +0100

    caught and seen ready

--- src/Pokemon/Pokemon.jsx
+++ src/Pokemon/Pokemon.jsx
@@ -1,12 +1,20 @@
 import styles from "./Pokemon.module.css"
+import {useState, useEffect} from "react";
 
 function Pokemon (props) {
+    const [seen, setSeen] = useState(false);
+    const [caught, setCaught] = useState(false);
+
+    useEffect(() => {
+    }, [])
+
     return (
         <div className={styles.entry}>
             {props.image && <img className={styles.image} src={props.image} />}
             <p className={styles.text}>{props.id}</p>
-            <p className={styles.text}>{props.name}</p>
-            <p className={styles.text}>{props.type}</p>
+            <p className={styles.text}>{caught ? props.name : "???"}</p>
+            <p className={styles.text}>Seen: {seen ? "Yes" : "No"}</p>
+            <p className={styles.text}>Caught: {caught ? "Yes" : "No"}</p>
         </div>
     )
 }
