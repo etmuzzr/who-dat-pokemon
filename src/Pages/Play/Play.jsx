d4ebde917f7abe35e8c397c1a8e8ccbbb13ec85e
Ethan <ethanmurray180@gmail.com>
2025-09-15 13:04:53 +0100

    Reapply "failed random implementation"
    This reverts commit 76db4e9c5dc33c015498c8e7f15c06c252dfbc09.

--- src/Pages/Play/Play.jsx
+++ src/Pages/Play/Play.jsx
@@ -1,29 +1,40 @@
 import styles from './Play.module.css';
 import Poke from '../../Pokemon/Poke.jsx';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import Type from '../../Pokemon/Type.jsx';
 
 function Play() {
 
     const [revealed, setRevealed] = useState(false);
-    const [randomId, setRandomId] = useState(1);
+    const [randomId, setRandomId] = useState(Math.floor(Math.random() * Poke.generations[9].end) + 1);
 
-    const resultName = Poke.fetchData(randomId).name.toLowerCase()
+    const resultData = Poke.fetchData(randomId);
 
     return (
         <>
         <div className={styles.play}>
-            {Poke.fetchPokemon(randomId, false, revealed)}  
-        </div>
-        <input className={styles.input} type="text" placeholder="Who dat Pokemon?" onChange={(e) => {
-            if (e.target.value.toLowerCase() === resultName) { //maybe add some better string matching (allow for spelling mistakes)
+            <div className={styles.wild}>
+                {resultData.image && <img className={revealed ? styles.seen : styles.notseen} src={resultData.image} />}
+                <p className={styles.text}>{revealed ? resultData.id : "???"}</p>
+                <p className={styles.text}>{revealed ? resultData.name : "???"}</p>
+                <div className={styles.types}>
+                    {resultData.types && resultData.types.map(t => <Type name={t.type.name} show={revealed} />)}
+                </div>
+            </div>
+            <input className={styles.input} type="text" placeholder="Who dat Pokemon?" onChange={(e) => {
+            if (e.target.value.toLowerCase() === resultData.name.toLowerCase()) { //maybe add some better string matching (allow for spelling mistakes)
                 setRevealed(true);
                 e.target.value = "";
                 e.target.disabled = true;
-                //show they got it correct
-                //show new pokemon
-                
+                setTimeout(() => {
+                    setRandomId(Math.floor(Math.random() * Poke.generations[9].end) + 1);
+                    setRevealed(false);
+                    e.target.disabled = false;
+                }, 2000);
             }
-        }}/>
+        }}/> 
+        </div>
+        
         </>
     );
 }
--- src/Pokemon/Pokemon.jsx
+++ src/Pokemon/Pokemon.jsx
@@ -3,47 +3,27 @@ import {useState, useEffect} from "react";
 import Type from "./Type.jsx"
 
 function Pokemon (props) {
-
-    //pokedex entry view
-    
-    if (props.fullDisplay) {
         
-        const [seen, setSeen] = useState(false);
-        const [caught, setCaught] = useState(false);
+    const [seen, setSeen] = useState(false);
+    const [caught, setCaught] = useState(false);
 
-        useEffect(() => {
-            //set seen/caught here based off cookies
+    useEffect(() => {
+        //set seen/caught here based off cookies
 
-        }, [])
-
-        return (
-            <div className={styles.entry}>
-                {props.image && <img className={seen ? styles.seen : styles.notseen} src={props.image} />}
-                <p className={styles.text}>{props.id}</p>
-                <p className={styles.text}>{caught ? props.name : "???"}</p>
-                <div className={styles.types}>
-                    {props.types && props.types.map(t => <Type name={t.type.name} show={seen} />)} 
-                </div>
-                <p className={styles.text}>Seen: {seen ? "Yes" : "No"}</p>
-                <p className={styles.text}>Caught: {caught ? "Yes" : "No"}</p>
-            </div> 
-        )   
-    }
-
-    //game view
+    }, [])
 
     return (
-        <>
-        <div className={styles.wild}>
-            {props.image && <img className={props.revealed ? styles.seen : styles.notseen} src={props.image} />}
-            <p className={styles.text}>{props.revealed ? props.id : "???"}</p>
-            <p className={styles.text}>{props.revealed ? props.name : "???"}</p>
+        <div className={styles.entry}>
+            {props.image && <img className={seen ? styles.seen : styles.notseen} src={props.image} />}
+            <p className={styles.text}>{props.id}</p>
+            <p className={styles.text}>{caught ? props.name : "???"}</p>
             <div className={styles.types}>
-                {props.types && props.types.map(t => <Type name={t.type.name} show={props.revealed} />)} 
+                {props.types && props.types.map(t => <Type name={t.type.name} show={seen} />)} 
             </div>
-        </div>
-        </>
-    );
+            <p className={styles.text}>Seen: {seen ? "Yes" : "No"}</p>
+            <p className={styles.text}>Caught: {caught ? "Yes" : "No"}</p>
+        </div> 
+    )       
 }
 
 export default Pokemon;
