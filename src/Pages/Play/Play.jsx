76db4e9c5dc33c015498c8e7f15c06c252dfbc09
Ethan <ethanmurray180@gmail.com>
2025-09-15 13:04:30 +0100

    Revert "failed random implementation"
    This reverts commit 4af677dce1744f5e5034a12a266fa1ade8f73069.

--- src/Pages/Play/Play.jsx
+++ src/Pages/Play/Play.jsx
@@ -1,40 +1,29 @@
 import styles from './Play.module.css';
 import Poke from '../../Pokemon/Poke.jsx';
-import {useState, useEffect} from 'react';
-import Type from '../../Pokemon/Type.jsx';
+import {useState} from 'react';
 
 function Play() {
 
     const [revealed, setRevealed] = useState(false);
-    const [randomId, setRandomId] = useState(Math.floor(Math.random() * Poke.generations[9].end) + 1);
+    const [randomId, setRandomId] = useState(1);
 
-    const resultData = Poke.fetchData(randomId);
+    const resultName = Poke.fetchData(randomId).name.toLowerCase()
 
     return (
         <>
         <div className={styles.play}>
-            <div className={styles.wild}>
-                {resultData.image && <img className={revealed ? styles.seen : styles.notseen} src={resultData.image} />}
-                <p className={styles.text}>{revealed ? resultData.id : "???"}</p>
-                <p className={styles.text}>{revealed ? resultData.name : "???"}</p>
-                <div className={styles.types}>
-                    {resultData.types && resultData.types.map(t => <Type name={t.type.name} show={revealed} />)}
-                </div>
-            </div>
-            <input className={styles.input} type="text" placeholder="Who dat Pokemon?" onChange={(e) => {
-            if (e.target.value.toLowerCase() === resultData.name.toLowerCase()) { //maybe add some better string matching (allow for spelling mistakes)
+            {Poke.fetchPokemon(randomId, false, revealed)}  
+        </div>
+        <input className={styles.input} type="text" placeholder="Who dat Pokemon?" onChange={(e) => {
+            if (e.target.value.toLowerCase() === resultName) { //maybe add some better string matching (allow for spelling mistakes)
                 setRevealed(true);
                 e.target.value = "";
                 e.target.disabled = true;
-                setTimeout(() => {
-                    setRandomId(Math.floor(Math.random() * Poke.generations[9].end) + 1);
-                    setRevealed(false);
-                    e.target.disabled = false;
-                }, 2000);
+                //show they got it correct
+                //show new pokemon
+                
             }
-        }}/> 
-        </div>
-        
+        }}/>
         </>
     );
 }
